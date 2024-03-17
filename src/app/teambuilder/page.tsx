d3bbea9a0f0b7d8fd79c5c2911b0587f8d1066e1
SkynixFr <romain.ludovic.day@gmail.com>
2024-03-17 17:07:29 +0100

    feat(team): user can add a team, display all teams

--- src/app/teambuilder/page.tsx
+++ src/app/teambuilder/page.tsx
@@ -1,7 +1,46 @@
+'use client';
+import { FormEvent, useState } from 'react';
+import axios from 'axios';
+
+// Types
+import Team from '../../types/Team';
+
 const TeamBuilding = () => {
+	const [teams, setTeams] = useState<Team[]>([]);
+	const [apiMessage, setApiMessage] = useState<string>('');
+	const addTeam = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const { value } = e.target[0];
+		const newTeam: Team = { name: value };
+		console.log(newTeam);
+		try {
+			await axios
+				.post('http://localhost:8080/api/v1/teams', newTeam)
+				.then(res => {
+					setTeams([...teams, newTeam]);
+					setApiMessage(res.data);
+				});
+		} catch (err) {
+			if (axios.isAxiosError(err)) {
+				setApiMessage(err.response.data);
+			}
+		}
+	};
 	return (
 		<div>
 			<h1>Team Builder</h1>
+			Create a team:{' '}
+			<form onSubmit={addTeam}>
+				<input type="text" placeholder="Team name" name="team" />
+				<button>Create</button>
+				{apiMessage && <p>{apiMessage}</p>}
+			</form>
+			Your teams:
+			<ul>
+				{teams.map(team => (
+					<li key={team.name}>{team.name}</li>
+				))}
+			</ul>
 		</div>
 	);
 };
