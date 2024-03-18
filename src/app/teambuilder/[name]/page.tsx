8b554c4a96701ed4fad3b46184ca261d6dd7fc7c
SkynixFr <romain.ludovic.day@gmail.com>
2024-03-18 23:22:29 +0100

    feat(team): get pokemons from pokeapi, paging handling

--- src/app/teambuilder/[name]/page.tsx
+++ src/app/teambuilder/[name]/page.tsx
@@ -2,12 +2,41 @@
 import Link from 'next/link';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+// Types
+import Pokemon from '../../../types/Pokemon';
 
 const pokemonBuilder = ({ params }: { params: { name: string } }) => {
 	const decodedName = decodeURIComponent(params.name);
 	const team = useSelector((state: RootState) =>
 		state.teams.teams.find(team => team.name === decodedName)
 	);
+	const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+	const [page, setPage] = useState<number>(1);
+	const limit = 9;
+
+	useEffect(() => {
+		(async () => {
+			try {
+				const response = await axios.get(
+					`https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${limit * (page - 1)}`
+				);
+				const promises = response.data.results.map(async (pokemon: any) => {
+					const pokemonResponseDetails = await axios.get(pokemon.url);
+					const { id, name, types } = pokemonResponseDetails.data;
+					return { id, name, types };
+				});
+				const resolvedPromises = await Promise.all(promises);
+				setPokemons(resolvedPromises);
+			} catch (err) {
+				if (axios.isAxiosError(err)) {
+					console.error(err);
+				}
+			}
+		})();
+	}, [page]);
 
 	return (
 		<div>
@@ -15,8 +44,39 @@ const pokemonBuilder = ({ params }: { params: { name: string } }) => {
 			<Link href="/teambuilder">Back</Link>
 			{team ? (
 				<div>
-					<p>Team: {team.name}</p>
-					<p>Update your team here</p>
+					<div>
+						<p>Team: {team.name}</p>
+						<p>Update your team here</p>
+					</div>
+
+					<h1>Pokemons: </h1>
+					{pokemons ? (
+						<ul>
+							{pokemons.map(pokemon => (
+								<li key={pokemon.id}>
+									<span>#{pokemon.id} </span>
+									{pokemon.name}
+									<ul>
+										{pokemon.types.map((type: any) => (
+											<li key={type.slot}>{type.type.name}</li>
+										))}
+									</ul>
+								</li>
+							))}
+						</ul>
+					) : (
+						<p>Loading...</p>
+					)}
+
+					<div>
+						<span>Page: {page}</span>
+						{page > 1 && (
+							<button onClick={() => setPage(page - 1)}>Previous</button>
+						)}
+						{page < 10 && (
+							<button onClick={() => setPage(page + 1)}>Next</button>
+						)}
+					</div>
 				</div>
 			) : (
 				<p>Loading...</p>
---
+++ src/types/Pokemon.ts
@@ -0,0 +1,30 @@
+type Pokemon = {
+	name: string;
+	url?: string;
+	id: number;
+	height?: number;
+	weight?: number;
+	abilities?: string[];
+	types?: string[];
+	stats?: {
+		hp: number;
+		attack: number;
+		defense: number;
+		specialAttack: number;
+		specialDefense: number;
+		speed: number;
+	};
+	image?: string;
+	moves?: string[];
+	species?: string;
+	base_experience?: number;
+	order?: number;
+	is_default?: boolean;
+	location_area_encounters?: string;
+	held_items?: string[];
+	game_indices?: string[];
+	forms?: string[];
+	flavor_text_entries?: string;
+};
+
+export default Pokemon;
