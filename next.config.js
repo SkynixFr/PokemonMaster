043b27968f1ea72f63ae6fce7f8cd71d4dc51dcb
SkynixFr <romain.ludovic.day@gmail.com>
2024-03-22 09:20:27 +0100

    update(config): update next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
-module.exports = {
+//** @type {import('next').NextConfig} */
+
+const nextConfig = {
 	images: {
-		domains: ['avatars.githubusercontent.com', 'raw.githubusercontent.com']
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'raw.githubusercontent.com',
+				port: ''
+			}
+		]
 	}
 };
+
+module.exports = nextConfig;
