5ad24fa47187ce691c4eec25fbf9ee2b2ad0116f
JoanE <joan.espias@gmail.com>
2024-04-09 15:58:28 +0200

    heal and atk handling for both players, minor correction

--- src/back/classes/pokemon.ts
+++ src/back/classes/pokemon.ts
@@ -9,28 +9,39 @@ class Pokemon implements IPokemon {
 	readonly name: string;
 	readonly stats: Stat[];
 	readonly moves: Move[];
+	readonly status?: Status;
 
-	constructor(name: string, stat: Stat[], moves: Move[]) {
+	constructor(name: string, stat: Stat[], moves: Move[], status?: Status) {
 		this.name = name;
 		this.stats = stat;
 		this.moves = moves;
+		this.status = status;
 	}
 
 	attack(opponentPokemon: Pokemon, move: Move): Pokemon {
 		const damage = move.power;
 		const opponentHp = opponentPokemon.stats.find(stat => stat.name === 'hp');
 		const updatedHp = opponentHp.decrease(damage);
+		const index = opponentPokemon.stats.findIndex(stat => stat.name === 'hp');
+		opponentPokemon.stats[index] = updatedHp;
 		return new Pokemon(
 			opponentPokemon.name,
-			[updatedHp],
+			opponentPokemon.stats,
 			opponentPokemon.moves
 		);
 	}
 
 	heal(): Pokemon {
 		const hp = this.stats.find(stat => stat.name === 'hp');
-		const updatedHp = hp.increase(20);
-		return new Pokemon(this.name, [updatedHp], this.moves);
+		const updatedHp = hp.increase(30);
+		const index = this.stats.findIndex(stat => stat.name === 'hp');
+		this.stats[index] = updatedHp;
+		console.log(this);
+		return new Pokemon(this.name, this.stats, this.moves);
+	}
+
+	changeStatus(status: Status): Pokemon {
+		return new Pokemon(this.name, this.stats, this.moves, status);
 	}
 }
 
