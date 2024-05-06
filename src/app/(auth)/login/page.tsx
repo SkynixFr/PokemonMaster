589967c67bc2b9f1332b417f7457ef296136704b
Ricky Khao <khaoricky@gmail.com>
2024-05-06 11:09:35 +0200

    feat(Login): add Login Page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,49 +1,14 @@
 'use client';
 import Link from 'next/link';
 import React, { useState } from 'react';
+import FormLogin from '../../../front/components/Login/formLogin';
 
-const Login: React.FC = () => {
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-
-	const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setEmail(e.target.value);
-	};
-
-	const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setPassword(e.target.value);
-	};
-
-	const handleSubmit = (e: React.FormEvent) => {
-		e.preventDefault();
-		// Ajoutez ici la logique de connexion
-	};
-
+const Login = () => {
 	return (
 		<div>
 			<Link href={'/'}>Go Back</Link>
 			<h1>Page de connexion</h1>
-			<form onSubmit={handleSubmit}>
-				<div>
-					<label htmlFor="email">Email:</label>
-					<input
-						type="email"
-						id="email"
-						value={email}
-						onChange={handleEmailChange}
-					/>
-				</div>
-				<div>
-					<label htmlFor="password">Mot de passe:</label>
-					<input
-						type="password"
-						id="password"
-						value={password}
-						onChange={handlePasswordChange}
-					/>
-				</div>
-				<button type="submit">Se connecter</button>
-			</form>
+			<FormLogin></FormLogin>
 		</div>
 	);
 };
--- src/front/actions/user.actions.ts
+++ src/front/actions/user.actions.ts
@@ -9,7 +9,6 @@ export const register = async (formData: FormData) => {
 		email: formData.get('email').toString(),
 		role: 'USER'
 	};
-	console.log(userCreate);
 
 	const response = await fetch('http://localhost:8080/api/v1/user/register', {
 		method: 'POST',
@@ -17,6 +16,23 @@ export const register = async (formData: FormData) => {
 			'Content-Type': 'application/json'
 		},
 		body: JSON.stringify(userCreate)
+	});
+	return response.json();
+};
+
+export const login = async (formData: FormData) => {
+	const userLogin = {
+		email: formData.get('email').toString(),
+		password: formData.get('password').toString()
+	};
+	console.log(userLogin);
+
+	const response = await fetch('http://localhost:8080/api/v1/user/login', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(userLogin)
 	});
 	return response.json();
 };
