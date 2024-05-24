e7bd7cbd37c8c67fed13ec73e8c996e4fa31e516
JoanE <joan.espias@gmail.com>
2024-05-24 16:16:34 +0200

    paralysis handling

--- src/app/(user)/battle/page.tsx
+++ src/app/(user)/battle/page.tsx
@@ -1,14 +1,12 @@
 'use client';
 
 import { use, useEffect, useState } from 'react';
-import Link from 'next/link';
 
 // Classes
 import PokemonClass from '../../../back/classes/pokemon';
 import BattleClass from '../../../back/classes/battle';
 import Move from '../../../back/classes/move';
 import Status from '../../../back/classes/status';
-import { set } from 'zod';
 
 // Interfaces
 interface BattleProps {
@@ -18,7 +16,7 @@ interface BattleProps {
 // Constants
 const MAX_TURNS_ASLEEP = 3;
 const THAW_CHANCE = 0.2;
-const PARALYSIS_CHANCE_TO_MOVE = 0.25;
+const PARALYSIS_MOVE_CHANCE = 0.25;
 
 const Battle = ({ battle }: BattleProps) => {
 	const [playerPokemon, setPlayerPokemon] = useState<PokemonClass | null>();
@@ -38,12 +36,16 @@ const Battle = ({ battle }: BattleProps) => {
 	const [opponentThawChance, setOpponentThawChance] = useState<number>(0);
 	const [poisonDamagesStep, setPoisonDamagesStep] = useState<boolean>(false);
 	const [burnDamagesStep, setBurnDamagesStep] = useState<boolean>(false);
-	const [playerAbleToMoveWhileParalyzed, setPlayerAbleToMoveWhileParalyzed] =
-		useState<boolean>(false);
+	const [playerMoveWhileParalyzedChance, setPlayerMoveWhileParalyzedChance] =
+		useState<number>(0);
 	const [
-		opponentAbleToMoveWhileParalyzed,
-		setOpponentAbleToMoveWhileParalyzed
-	] = useState<boolean>(false);
+		opponentMoveWhileParalyzedChance,
+		setOpponentMoveWhileParalyzedChance
+	] = useState<number>(0);
+	const [playerMoveWhileParalyzed, setPlayerMoveWhileParalyzed] =
+		useState<boolean>(false);
+	const [opponentMoveWhileParalyzed, setOpponentMoveWhileParalyzed] =
+		useState<boolean>(false);
 
 	// Récupération des pokémons depuis le localStorage ou la room
 	useEffect(() => {
@@ -127,7 +129,25 @@ const Battle = ({ battle }: BattleProps) => {
 		}
 	}, [playerThawChance, opponentThawChance]);
 
-	// Déclenchement des dégâts de poison
+	// 25% de chances d'attaquer sous statut Paralysie
+	useEffect(() => {
+		setPlayerMoveWhileParalyzed(false);
+		setOpponentMoveWhileParalyzed(false);
+		if (
+			playerPokemon?.status.name === 'PAR' &&
+			playerMoveWhileParalyzedChance > PARALYSIS_MOVE_CHANCE
+		) {
+			setPlayerMoveWhileParalyzed(true);
+		}
+		if (
+			opponentPokemon?.status.name === 'PAR' &&
+			opponentMoveWhileParalyzedChance > PARALYSIS_MOVE_CHANCE
+		) {
+			setOpponentMoveWhileParalyzed(true);
+		}
+	}, [playerMoveWhileParalyzedChance, opponentMoveWhileParalyzedChance]);
+
+	// Déclenchement des dégâts de poison et/ou de brûlure
 	useEffect(() => {
 		if (!poisonDamagesStep && !burnDamagesStep) return;
 		if (playerPokemon.status.name === 'PSN' || 'BRN') {
@@ -149,7 +169,7 @@ const Battle = ({ battle }: BattleProps) => {
 		setPoisonDamagesStep(false);
 	}, [poisonDamagesStep, burnDamagesStep]);
 
-	// Déroulement d'un tour de jeu
+	// Déroulement des attaques une fois les 2 joueurs prêts
 	useEffect(() => {
 		if (!playerReady || !opponentReady) return;
 		handleSleepStatus();
@@ -208,16 +228,10 @@ const Battle = ({ battle }: BattleProps) => {
 
 	const handleParalyzisStatus = () => {
 		if (playerPokemon.status.name === 'PAR') {
-			const random = Math.random();
-			if (random >= PARALYSIS_CHANCE_TO_MOVE) {
-				setPlayerAbleToMoveWhileParalyzed(true);
-			}
+			setPlayerMoveWhileParalyzedChance(Math.random());
 		}
 		if (opponentPokemon.status.name === 'PAR') {
-			const random = Math.random();
-			if (random >= PARALYSIS_CHANCE_TO_MOVE) {
-				setOpponentAbleToMoveWhileParalyzed(true);
-			}
+			setOpponentMoveWhileParalyzedChance(Math.random());
 		}
 	};
 
@@ -254,16 +268,11 @@ const Battle = ({ battle }: BattleProps) => {
 		}
 		setPlayerReady(false);
 		setOpponentReady(false);
-		setPlayerAbleToMoveWhileParalyzed(false);
-		setOpponentAbleToMoveWhileParalyzed(false);
 	};
 
 	// Gestion du move du joueur
 	const handlePlayerAttack = () => {
-		if (
-			playerPokemon.status.name === 'PAR' &&
-			!playerAbleToMoveWhileParalyzed
-		)
+		if (playerPokemon.status.name === 'PAR' && !playerMoveWhileParalyzed)
 			return opponentPokemon;
 		const updatedOpponentPokemon = playerPokemon.attack(
 			opponentPokemon,
@@ -279,10 +288,7 @@ const Battle = ({ battle }: BattleProps) => {
 
 	// Gestion du move de l'adversaire
 	const handleOpponentAttack = () => {
-		if (
-			opponentPokemon.status.name === 'PAR' &&
-			!opponentAbleToMoveWhileParalyzed
-		)
+		if (opponentPokemon.status.name === 'PAR' && !opponentMoveWhileParalyzed)
 			return playerPokemon;
 		const updatedPlayerPokemon = opponentPokemon.attack(
 			playerPokemon,
@@ -398,354 +404,3 @@ const Battle = ({ battle }: BattleProps) => {
 	);
 };
 export default Battle;
-
-// 'use client';
-// import { useSelector } from 'react-redux';
-// import { RootState } from '../store/store';
-// import { useEffect, useState } from 'react';
-//
-// // Icons
-// import { CloudRainWind, Timer } from 'lucide-react';
-//
-// // Components
-// import CustomImage from '../components/customImage';
-// import BattleTeam from '../components/battleTeam';
-// import BattlePokemon from '../components/battlePokemon';
-// import CustomButton from '../components/customButton';
-//
-// // Types
-// import BattleEffect from '../../types/BattleEffect';
-//
-// // POO
-// import PokemonClass from '../../classes/Pokemon';
-// import BattleClass from '../../classes/Battle';
-// import TeamClass from '../../classes/Team';
-// import StatsClass from '../../classes/Stats';
-//
-// const Battle = () => {
-// 	const [hostTeam, setHostTeam] = useState<TeamClass | null>(null);
-// 	const [guestTeam, setGuestTeam] = useState<TeamClass | null>(null);
-// 	const [currentHp, setCurrentHp] = useState<number>(guestTeam?.getActivePokemon().stats.currentHp);
-// 	const [hostTeamBattleEffects, setBattleEffectsTeamOne] = useState<
-// 		BattleEffect[]
-// 	>([
-// 		{ name: 'BRU', type: 'status' },
-// 		{ name: 'Atk', number: 0.67, type: 'debuff' },
-// 		{ name: 'Atk', number: 0.67, type: 'debuff' },
-// 		{ name: 'Atk', number: 0.67, type: 'debuff' },
-// 		{ name: 'Atk', number: 0.67, type: 'debuff' },
-// 		{ name: 'Atk', number: 0.67, type: 'debuff' },
-// 		{ name: 'Atk', number: 0.67, type: 'debuff' },
-// 		{ name: 'Atk', number: 0.67, type: 'debuff' },
-// 		{ name: 'Atk', number: 0.67, type: 'debuff' }
-// 	]);
-// 	const [guestTeamBattleEffects, setBattleEffectsTeamTwo] = useState<
-// 		BattleEffect[]
-// 	>([
-// 		{ name: 'SpD', number: 1.5, type: 'buff' },
-// 		{ name: 'PAR', type: 'status' }
-// 	]);
-// 	const [globalEffect, setGlobalEffect] = useState<BattleEffect[]>([
-// 		{ name: 'Rain', turns: 5 }
-// 	]);
-//
-// 	const [theme, setTheme] = useState<string>('day');
-// 	const toggleTheme = () => {
-// 		setTheme(theme === 'day' ? 'night' : 'day');
-// 	};
-//
-// 	useEffect(() => {
-//
-// 		const stats = new StatsClass({
-// 			hp: 45,
-// 			attack: 49,
-// 			defense: 49,
-// 			spAttack: 65,
-// 			spDefense: 65,
-// 			speed: 45,
-// 			ev: 0,
-// 			iv: 0,
-// 			currentHp: 45
-// 		});
-//
-// 		const bulbasaur = new PokemonClass({
-// 			id: 1,
-// 			name: 'Bulbasaur',
-// 			stats,
-// 			moves: [
-// 				{
-// 					name: 'Tackle',
-// 					type: { name: 'normal' },
-// 					category: 'physical',
-// 					power: 40,
-// 					accuracy: 100,
-// 					pp: 35,
-// 					description:
-// 						'A physical attack in which the user charges and slams into the target with its whole body.',
-// 					effect: 'no effect'
-// 				}
-// 			]
-// 		});
-// 		const squirtle = new PokemonClass({
-// 			id: 7,
-// 			name: 'Squirtle',
-// 			stats,
-// 			moves: [
-// 				{
-// 					name: 'Tackle',
-// 					type: { name: 'normal' },
-// 					category: 'physical',
-// 					power: 40,
-// 					accuracy: 100,
-// 					pp: 35,
-// 					description:
-// 						'A physical attack in which the user charges and slams into the target with its whole body.',
-// 					effect: 'no effect'
-// 				}
-// 			]
-// 		});
-//
-// 		const charmander = new PokemonClass({
-// 			id: 4,
-// 			name: 'Charmander',
-// 			stats,
-// 			moves: [
-// 				{
-// 					name: 'Tackle',
-// 					type: { name: 'normal' },
-// 					category: 'physical',
-// 					power: 40,
-// 					accuracy: 100,
-// 					pp: 35,
-// 					description:
-// 						'A physical attack in which the user charges and slams into the target with its whole body.',
-// 					effect: 'no effect'
-// 				}
-// 			]
-// 		});
-//
-// 		const mew = new PokemonClass({
-// 			id: 151,
-// 			name: 'Mew',
-// 			stats,
-// 			moves: [
-// 				{
-// 					name: 'Tackle',
-// 					type: { name: 'normal' },
-// 					category: 'physical',
-// 					power: 40,
-// 					accuracy: 100,
-// 					pp: 35,
-// 					description:
-// 						'A physical attack in which the user charges and slams into the target with its whole body.',
-// 					effect: 'no effect'
-// 				}
-// 			]
-// 		});
-//
-// 		const arcanine = new PokemonClass({
-// 			id: 59,
-// 			name: 'Arcanine',
-// 			stats,
-// 			moves: [
-// 				{
-// 					name: 'Tackle',
-// 					type: { name: 'normal' },
-// 					category: 'physical',
-// 					power: 40,
-// 					accuracy: 100,
-// 					pp: 35,
-// 					description:
-// 						'A physical attack in which the user charges and slams into the target with its whole body.',
-// 					effect: 'no effect'
-// 				}
-// 			]
-// 		});
-//
-// 		const ninetails = new PokemonClass({
-// 			id: 38,
-// 			name: 'Ninetails',
-// 			stats,
-// 			moves: [
-// 				{
-// 					name: 'Tackle',
-// 					type: { name: 'normal' },
-// 					category: 'physical',
-// 					power: 40,
-// 					accuracy: 100,
-// 					pp: 35,
-// 					description:
-// 						'A physical attack in which the user charges and slams into the target with its whole body.',
-// 					effect: 'no effect'
-// 				}
-// 			]
-// 		});
-//
-// 		const hostTeam = new TeamClass(
-// 			{
-// 				name: 'Host',
-// 				avatar: '/images/avatars/cynthia.png',
-// 				pokemons: [bulbasaur, squirtle, charmander, mew, arcanine, ninetails]
-// 			},
-// 			bulbasaur
-// 		);
-//
-// 		setHostTeam(hostTeam);
-//
-// 		const guestTeam = new TeamClass(
-// 			{
-// 				name: 'Guest',
-// 				avatar: '/images/avatars/red.png',
-// 				pokemons: [squirtle, charmander, bulbasaur, ninetails, mew, arcanine]
-// 			},
-// 			squirtle
-// 		);
-//
-// 		setGuestTeam(guestTeam);
-//
-// 		console.log(guestTeam.getActivePokemon());
-//
-// 		const battle = new BattleClass(hostTeam, guestTeam, 1);
-// 	}, []);
-//
-// 	if (
-// 		!guestTeam ||
-// 		!hostTeam ||
-// 		!guestTeam.getActivePokemon() ||
-// 		!hostTeam.getActivePokemon()
-// 	) {
-// 		return <div>Loading...</div>;
-// 	}
-//
-// 	return (
-// 		<div className="battle-container">
-// 			<CustomImage
-// 				src={`/images/backgrounds/bg-battle-${theme}-upscale-tiny.jpg`}
-// 				alt="Battle background"
-// 				fill={true}
-// 				priority={true}
-// 				objectFit="cover"
-// 				className="battle-background"
-// 			/>
-// 			<div className="battle-theme">
-// 				<CustomButton
-// 					icon={
-// 						theme === 'day'
-// 							? { name: 'Moon', size: 20 }
-// 							: { name: 'Sun', size: 20 }
-// 					}
-// 					onClick={toggleTheme}
-// 					className="theme-icon"
-// 				/>
-// 			</div>
-// 			<div className="battle-turn">Turn 1</div>
-//
-// 			<div className="battle-toast">Toast</div>
-//
-// 			<div className="battle-global-infos">
-// 				{globalEffect &&
-// 					globalEffect.map(effect => (
-// 						<>
-// 							{effect.name === 'Rain' && (
-// 								<div className="rain" key={effect.name}>
-// 									<CloudRainWind size={20} />
-// 									<span>{effect.turns}</span>
-// 								</div>
-// 							)}
-// 						</>
-// 					))}
-// 			</div>
-//
-// 			<div className="battle-team player">
-// 				<BattleTeam team={hostTeam} />
-// 			</div>
-//
-// 			<div className="battle-team opponent">
-// 				<BattleTeam team={guestTeam} />
-// 			</div>
-//
-// 			<div className="battle-pokemon player">
-// 				<BattlePokemon
-// 					activePokemon={hostTeam.getActivePokemon()}
-// 					battleEffects={hostTeamBattleEffects}
-// 					player={true}
-// 				/>
-// 			</div>
-//
-// 			<div className="battle-pokemon opponent">
-// 				<BattlePokemon
-// 					activePokemon={guestTeam.getActivePokemon()}
-// 					battleEffects={guestTeamBattleEffects}
-// 				/>
-// 			</div>
-//
-// 			<div className="battle-actions">
-// 				<div>
-// 					<CustomButton
-// 						text="Attack"
-// 						image={{
-// 							src: '/images/icons/battle.png',
-// 							alt: 'Battle icon',
-// 							width: 75,
-// 							height: 75,
-// 							priority: true,
-// 							className: 'battle-icon'
-// 						}}
-// 						onClick={() => {setCurrentHp(hostTeam.getActivePokemon().attack(guestTeam.getActivePokemon(), hostTeam.getActivePokemon().moves[0]))
-// 						guestTeam.getActivePokemon().stats.currentHp = currentHp;
-// 						console.log('guest:', guestTeam.getActivePokemon().stats.currentHp)
-// 						console.log('host:', hostTeam.getActivePokemon().stats.currentHp)
-// 					}}
-// 					/>
-// 					<CustomButton
-// 						text="Team"
-// 						image={{
-// 							src: '/images/icons/pokeball.png',
-// 							alt: 'Battle icon',
-// 							width: 75,
-// 							height: 75,
-// 							priority: true,
-// 							className: 'battle-icon'
-// 						}}
-// 					/>
-// 					<CustomButton
-// 						text="Chat"
-// 						image={{
-// 							src: '/images/icons/chat.png',
-// 							alt: 'Battle icon',
-// 							width: 75,
-// 							height: 75,
-// 							priority: true,
-// 							className: 'battle-icon'
-// 						}}
-// 					/>
-// 					<CustomButton
-// 						text="Surrender"
-// 						image={{
-// 							src: '/images/icons/run.png',
-// 							alt: 'Battle icon',
-// 							width: 75,
-// 							height: 75,
-// 							priority: true,
-// 							className: 'battle-icon run'
-// 						}}
-// 					/>
-// 				</div>
-// 			</div>
-//
-// 			<div className="battle-timer">
-// 				<CustomButton
-// 					icon={{
-// 						name: 'Timer',
-// 						size: 20,
-// 						strokeWidth: 3
-// 					}}
-// 					text="Start the timer"
-// 					className="timer-icon"
-// 				/>
-// 			</div>
-// 		</div>
-// 	);
-// };
-//
-// export default Battle;
